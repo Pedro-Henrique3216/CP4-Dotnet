9cf16e7ed8de125da29ab0323f2e84e42010cb1e
Pedro Henrique <ph6704938@gmail.com>
2025-09-07 18:22:33 -0300

    feat: add Log class with validation for message property

---
+++ MottuChallenge.Domain/Entities/Log.cs
@@ -0,0 +1,40 @@
+﻿using MottuChallenge.Domain.Exceptions;
+using MottuChallenge.Domain.Validations;
+
+namespace MottuChallenge.Domain.Entities
+{
+    public class Log
+    {
+        public Guid Id { get; private set; }
+        public string Message { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public Guid MotorcycleId { get; private set; }
+        public Motorcycle Motorcycle { get; private set; }
+        public Spot PreviousSpot { get; private set; }
+        public Guid PreviousSpotId { get; private set; }
+        public Spot DestinationSpot { get; private set; }
+        public Guid DestinationSpotId { get; private set; }
+
+        public Log(string message, Guid motorcycleId, Guid previousSpotId, Guid destinationSpotId)
+        {
+            ValidateMessage(message);
+            this.Id = Guid.NewGuid();
+            this.Message = message;
+            this.CreatedAt = DateTime.UtcNow;
+            this.MotorcycleId = motorcycleId;
+            this.PreviousSpotId = previousSpotId;
+            this.DestinationSpotId = destinationSpotId;
+        }
+
+        public Log() { }
+
+        private void ValidateMessage(string message)
+        {
+            Guard.AgainstNullOrWhitespace(message, nameof(message), nameof(Log));
+            if (message.Length > 150)
+            {
+                throw new DomainValidationException("Message must have less than 150 characters", nameof(message), nameof(Log));
+            }
+        }
+    }
+}
