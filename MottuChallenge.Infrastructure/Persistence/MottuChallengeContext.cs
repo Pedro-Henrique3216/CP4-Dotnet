803cd2bbc19182dfe50f7fbacc6c3788fcc19986
Pedro Henrique <ph6704938@gmail.com>
2025-09-08 20:32:47 -0300

    feat: add mysqlPackage and add MottuChallengeContext

---
+++ MottuChallenge.Infrastructure/Persistence/MottuChallengeContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Infrastructure.Persistence
+{
+    internal class MottuChallengeContext(DbContextOptions<MottuChallengeContext> options) : DbContext(options)
+    {
+        public DbSet<Yard> Yards { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
