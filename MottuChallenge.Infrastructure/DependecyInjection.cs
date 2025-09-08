b950493a14a52eb1201c60801e7cba4ef5cf9c13
Pedro Henrique <ph6704938@gmail.com>
2025-09-08 20:49:59 -0300

    feat: add DependencyInjection class for MySQL DbContext setup

---
+++ MottuChallenge.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MottuChallenge.Infrastructure.Persistence;
+
+namespace MottuChallenge.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MottuChallengeContext>(options =>
+            {
+                options.UseMySQL(configuration.GetConnectionString("MySqlConnection"));
+            });
+
+            return services;
+        } 
+    }
+}
