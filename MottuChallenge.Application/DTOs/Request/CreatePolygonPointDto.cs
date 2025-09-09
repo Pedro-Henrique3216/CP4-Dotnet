a95583ca37d39e8db0e9a01a7a5232af875f2a48
Pedro Henrique <ph6704938@gmail.com>
2025-09-09 19:49:42 -0300

    feat: add DTO classes for polygon points and yards

---
+++ MottuChallenge.Application/DTOs/Request/CreatePolygonPointDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MottuChallenge.Application.DTOs.Request
+{
+    public class CreatePolygonPointDto
+    {
+        public int PointOrder { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
