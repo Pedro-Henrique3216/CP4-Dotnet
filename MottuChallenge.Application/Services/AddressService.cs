6338eeabdbb59b98249bfa64b0d416d5844f1193
Pedro Henrique <ph6704938@gmail.com>
2025-09-09 19:49:12 -0300

    feat: add ViaCep integration for address retrieval

---
+++ MottuChallenge.Application/Services/AddressService.cs
@@ -0,0 +1,32 @@
+﻿using MottuChallenge.Application.DTOs.Response;
+using MottuChallenge.Domain.Entities;
+using System.Text.Json;
+
+namespace MottuChallenge.Application.Services
+{
+    internal class AddressService(HttpClient httpClient) : IAddressService
+    {
+
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<Address> GetAddressByCepAsync(string cep, string number)
+        {
+            var url = $"https://viacep.com.br/ws/{cep}/json/";
+            var response = await _httpClient.GetStringAsync(url);
+
+            var viacepResponse = JsonSerializer.Deserialize<ViaCepResponse>(response)
+                                 ?? throw new Exception("CEP não encontrado");
+
+            return new Address
+            (
+                viacepResponse.Logradouro,
+                int.Parse(number),
+                viacepResponse.Bairro,
+                viacepResponse.Localidade,
+                viacepResponse.Uf,
+                cep,
+                "Brasil"
+            );
+        }
+    }
+}
---
+++ MottuChallenge.Application/Services/IAddressService.cs
@@ -0,0 +1,9 @@
+﻿using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Application.Services
+{
+    internal interface IAddressService
+    {
+        public Task<Address> GetAddressByCepAsync(string cep, string number);
+    }
+}
