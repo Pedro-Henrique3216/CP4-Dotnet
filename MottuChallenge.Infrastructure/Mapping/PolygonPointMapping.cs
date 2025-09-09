1f5b0bc1b30ae3ddd5b37dbb41c37785baca7ec2
Pedro Henrique <ph6704938@gmail.com>
2025-09-08 22:44:55 -0300

    feat: add PolygonPoint mapping

---
+++ MottuChallenge.Infrastructure/Mapping/PolygonPointMapping.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Infrastructure.Mapping
+{
+    internal class PolygonPointMapping : IEntityTypeConfiguration<PolygonPoint>
+    {
+        public void Configure(EntityTypeBuilder<PolygonPoint> builder)
+        {
+            builder.ToTable("polygon_points");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id)
+                   .HasColumnName("id")
+                   .ValueGeneratedOnAdd()
+                   .IsRequired();
+
+            builder.Property(p => p.PointOrder)
+                   .HasColumnName("point_order")
+                   .IsRequired();
+
+            builder.Property(p => p.X)
+                   .HasColumnName("x")
+                   .IsRequired();
+
+            builder.Property(p => p.Y)
+                   .HasColumnName("y")
+                   .IsRequired();
+
+            builder.Property(p => p.YardId)
+                   .HasColumnName("yard_id");
+
+            builder.Property(p => p.SectorId)
+                   .HasColumnName("sector_id");
+
+            builder.HasOne(p => p.Yard)
+                   .WithMany(y => y.Points)
+                   .HasForeignKey(p => p.YardId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(p => p.Sector)
+                   .WithMany(s => s.Points)
+                   .HasForeignKey(p => p.SectorId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
--- MottuChallenge.Infrastructure/Persistence/MottuChallengeContext.cs
+++ MottuChallenge.Infrastructure/Persistence/MottuChallengeContext.cs
@@ -8,11 +8,13 @@ namespace MottuChallenge.Infrastructure.Persistence
     {
         public DbSet<Yard> Yards { get; set; }
         public DbSet<Address> Addresses { get; set; }
+        public DbSet<PolygonPoint> PolygonPoints { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new YardMapping());
             modelBuilder.ApplyConfiguration(new AddressMapping());
+            modelBuilder.ApplyConfiguration(new PolygonPointMapping());
         }
     }
 }
