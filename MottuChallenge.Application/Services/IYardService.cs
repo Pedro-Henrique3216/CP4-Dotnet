f99edee8a8bbb3ea4ad6eff7c1794ab6104c6e6f
Pedro Henrique <ph6704938@gmail.com>
2025-09-09 22:05:49 -0300

    feat: create yard service with method to save Yard

---
+++ MottuChallenge.Application/Services/IYardService.cs
@@ -0,0 +1,10 @@
+﻿using MottuChallenge.Application.DTOs.Request;
+using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Application.Services
+{
+    public interface IYardService
+    {
+        Task<Yard> SaveYardAsync(CreateYardDto createYardDto);    
+    }
+}
---
+++ MottuChallenge.Application/Services/YardService.cs
@@ -0,0 +1,32 @@
+﻿using MottuChallenge.Application.DTOs.Request;
+using MottuChallenge.Domain.Entities;
+using MottuChallenge.Infrastructure.Repositories;
+
+namespace MottuChallenge.Application.Services
+{
+    public class YardService(IYardRepository yardRepository, IAddressService addressService) : IYardService
+    {
+
+        private readonly IYardRepository _yardRepository = yardRepository;
+        private readonly IAddressService _addressService = addressService;
+
+        public async Task<Yard> SaveYardAsync(CreateYardDto createYardDto)
+        {
+            var address = await _addressService.GetAddressByCepAsync(createYardDto.Cep, createYardDto.Number);
+
+            var yard = new Yard(createYardDto.Name, address.Id)
+            {
+                Address = address
+            };
+
+            foreach (var pointDto in createYardDto.Points)
+            {
+                var point = new PolygonPoint(pointDto.PointOrder, pointDto.X, pointDto.Y, yard.Id, Guid.Empty);
+                yard.AddPoint(point);
+            }
+            
+            await _yardRepository.SaveYardAsync(yard);
+            return yard;
+        }
+    }
+}
