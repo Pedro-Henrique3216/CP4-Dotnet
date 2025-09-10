517586c2e66733c39bba3e89c739deeb569ecfcc
Pedro Henrique <ph6704938@gmail.com>
2025-09-09 22:15:55 -0300

    feat:  add YardController with endpoint to save yard

---
+++ MottuChallenge.Api/Controllers/YardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MottuChallenge.Application.DTOs.Request;
+using MottuChallenge.Application.Services;
+
+namespace MottuChallenge.Api.Controllers
+{
+    [Route("api/yards")]
+    [ApiController]
+    public class YardController(IYardService yardService) : ControllerBase
+    {
+        private readonly IYardService _yardService = yardService;
+
+        [HttpPost]
+        public async Task Post([FromBody] CreateYardDto createYardDto)
+        {
+            await _yardService.SaveYardAsync(createYardDto);
+        }
+
+    }
+}
