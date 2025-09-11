210e986abf7540b68db7b977ed36f439cebd8a4f
Pedro Henrique <ph6704938@gmail.com>
2025-09-11 20:32:43 -0300

    feat: create method to generate spots in sectors

---
+++ MottuChallenge.Application/Services/SpotService.cs
@@ -0,0 +1,37 @@
+﻿using MottuChallenge.Application.Helpers;
+using MottuChallenge.Domain.Entities;
+using System.Numerics;
+
+namespace MottuChallenge.Application.Services
+{
+    public class SpotService : ISpotService
+    {
+        public List<Spot> GenerateSpot(Sector sector, double width, double height)
+        {
+            var spots = new List<Spot>();
+
+            double minX = sector.Points.Min(p => p.X);
+            double maxX = sector.Points.Max(p => p.X);
+            double minY = sector.Points.Min(p => p.Y);
+            double maxY = sector.Points.Max(p => p.Y);
+
+            double y = minY;
+
+            while (y <= maxY)
+            {
+                double x = minX;
+                while (x <= maxX)
+                {
+                    if (GeometryHelper.IsPointInsidePolygon(x, y, sector.Points.ToList()))
+                    {
+                        spots.Add(new Spot(x, y, sector.Id));
+                    }
+                    x += width;
+                }
+                y += height;
+            }
+
+            return spots;
+        }
+    }
+}
