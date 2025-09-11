187141a291a43df523248b83c28b452f28d4e798
Pedro Henrique <ph6704938@gmail.com>
2025-09-11 20:38:05 -0300

    feat: create enpoint to save sector

---
+++ MottuChallenge.Api/Controllers/SectorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MottuChallenge.Application.DTOs.Request;
+using MottuChallenge.Application.Services;
+
+namespace MottuChallenge.Api.Controllers
+{
+    [Route("api/sectors")]
+    [ApiController]
+    public class SectorController(ISectorService sectorService) : ControllerBase
+    {
+        private readonly ISectorService _sectorService = sectorService;
+
+        [HttpPost]
+        public async Task Post([FromBody] SectorCreateDto sectorCreateDto)
+        {
+            await _sectorService.SaveSectorAsync(sectorCreateDto);
+        }
+
+    }
+}
