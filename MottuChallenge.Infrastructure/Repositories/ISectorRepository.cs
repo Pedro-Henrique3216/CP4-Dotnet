3970db63b20c14503445f6bfb04cde409ffabbcb
Pedro Henrique <ph6704938@gmail.com>
2025-09-11 20:27:31 -0300

    feat: add sectorRepository with method to save in database

---
+++ MottuChallenge.Infrastructure/Repositories/ISectorRepository.cs
@@ -0,0 +1,12 @@
+﻿using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Infrastructure.Repositories
+{
+    public interface ISectorRepository
+    {
+
+        public Task<Sector> SaveSectorAsync(Sector sector);
+
+        public Task<List<Sector>> GetSectorsByYardIdAsync(Guid yardId);
+    }
+}
---
+++ MottuChallenge.Infrastructure/Repositories/SectorRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MottuChallenge.Domain.Entities;
+using MottuChallenge.Infrastructure.Persistence;
+
+namespace MottuChallenge.Infrastructure.Repositories
+{
+    internal class SectorRepository(MottuChallengeContext context) : ISectorRepository
+    {
+
+        private readonly MottuChallengeContext _context = context;
+
+        public async Task<Sector> SaveSectorAsync(Sector sector)
+        {
+            await _context.Sectors.AddAsync(sector);
+            await _context.SaveChangesAsync();
+            return sector;
+        }
+
+        public async Task<List<Sector>> GetSectorsByYardIdAsync(Guid yardId)
+        {
+            return await _context.Sectors
+                                 .Where(s => s.YardId == yardId)
+                                 .ToListAsync();
+        }
+    }
+}
