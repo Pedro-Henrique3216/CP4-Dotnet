ae47c24086c2a860a1b60510d2548dba29d0a83d
Pedro Henrique <ph6704938@gmail.com>
2025-09-11 20:28:44 -0300

    feat: create SectorType with method to save sectorType and findByName

---
+++ MottuChallenge.Infrastructure/Repositories/ISectorTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MottuChallenge.Domain.Entities;
+
+namespace MottuChallenge.Infrastructure.Repositories
+{
+    public interface ISectorTypeRepository
+    {
+
+        public Task<SectorType> SaveSectorTypeAsync(SectorType sectorType);
+
+        public Task<SectorType> FindSectorByName(string name);
+
+    }
+}
---
+++ MottuChallenge.Infrastructure/Repositories/SectorTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MottuChallenge.Domain.Entities;
+using MottuChallenge.Infrastructure.Persistence;
+
+namespace MottuChallenge.Infrastructure.Repositories
+{
+    internal class SectorTypeRepository(MottuChallengeContext context) : ISectorTypeRepository
+    {
+
+        private readonly MottuChallengeContext _context = context;
+
+        public async Task<SectorType> FindSectorByName(string name)
+        {
+            return await _context.SectorTypes
+                        .FirstOrDefaultAsync(s => s.Name == name);
+
+        }
+
+        public async Task<SectorType> SaveSectorTypeAsync(SectorType sectorType)
+        {
+            await _context.SectorTypes.AddAsync(sectorType);
+            await _context.SaveChangesAsync();
+            return sectorType;
+        }
+
+
+    }
+}
