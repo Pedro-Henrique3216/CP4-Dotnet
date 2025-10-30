85ff7fa07fb354d013adb47bdc227427cab54ee2
Pedro Henrique <ph6704938@gmail.com>
2025-10-29 23:26:58 -0300

    refactor: remove unnecessary private field in repository classes

--- MottuChallenge.Infrastructure/Repositories/SectorRepository.cs
+++ MottuChallenge.Infrastructure/Repositories/SectorRepository.cs
@@ -6,33 +6,30 @@ namespace MottuChallenge.Infrastructure.Repositories
 {
     internal class SectorRepository(MottuChallengeContext context) : ISectorRepository
     {
-
-        private readonly MottuChallengeContext _context = context;
-
         public async Task<Sector> SaveSectorAsync(Sector sector)
         {
-            await _context.Sectors.AddAsync(sector);
-            await _context.SaveChangesAsync();
+            await context.Sectors.AddAsync(sector);
+            await context.SaveChangesAsync();
             return sector;
         }
 
         public async Task<List<Sector>> GetSectorsByYardIdAsync(Guid yardId)
         {
-            return await _context.Sectors
+            return await context.Sectors
                                  .Where(s => s.YardId == yardId)
                                  .ToListAsync();
         }
 
         public async Task<Sector> GetSectorByIdAsync(Guid sectorId)
         {
-            return await _context.Sectors
+            return await context.Sectors
                                  .Include(s => s.Spots)
                                  .FirstOrDefaultAsync(s => s.Id == sectorId);
         }
 
         public async Task<List<Sector>> GetAllSectorsAsync()
         {
-            return await _context.Sectors
+            return await context.Sectors
                                  .Include(s => s.Spots)
                                  .ToListAsync();
         }
